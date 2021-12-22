3a4d3c067f1837875964761c2a38f1940993bb24
alzyoodlara32 <96166459+alzyoodlara32@users.noreply.github.com>
2021-12-22 09:15:58 +0200

    Create accordionData.js

---
+++ src/accordionData.js
@@ -0,0 +1,31 @@
+export const accordionData = [
+    {
+      title: 'Astronomy Or Astrology',
+      content: `Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+      ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+      amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+      odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.`
+  },
+  {
+      title: 'When The Morning Dawns',
+      content: `Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+      ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+      amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+      odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.`
+  },
+  {
+      title: 'Vacation Home Rental Success',
+      content: `Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+      ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+      amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+      odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.`
+  },
+  {
+  title: 'The Amazing Hubble',
+      content: `Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+      ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+      amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+      odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.`
+}
+
+];
